9fc438bb9fe2878476d5f64c8df44ce7698d76ff
averin-a66 <47901916+averin-a66@users.noreply.github.com>
2021-11-03 09:57:10 +0300

    Создание скрипта 1

--- media/tableEditor.js
+++ media/tableEditor.js
@@ -35,13 +35,23 @@ var vscode;
 				columnPropertiesContainer.innerHTML = message.properties;
 				return;
 
+      case 'set-active-page':
+        SelectTab(message.indexPage);
+        return;
+
+      case 'view-sql-text':
+        viewSqlText(message.sql, message.countRow);
+        return;
+
+      case 'do-save_data':
+        return;
 		}
 	});
 
 }());
 
 function SelectTab(idx) { //idx - выбранная вкладка
-  const tabCount = 7; //Всего вкладок
+  const tabCount = 8; //Всего вкладок
   for (let i = 1; i <= tabCount; ++i) {
     const _tab = /** @type {HTMLElement} */(document.getElementById("tab" + i)); //Div - содержимое вкладки i
     const _tabTitle = /** @type {HTMLElement} */(document.getElementById("tab-title" + i)); //Li - вкладка i
@@ -72,3 +82,42 @@ function doFocusedField(/**@type {string}*/ fieldId ) {
     });
   }
 }
+
+function doChangeEditor(/**@type {string}*/ id) {
+  const _editor = /** @type {HTMLInputElement} */(document.getElementById(id));
+  const _ins = /** @type {HTMLElement} */document.querySelector('.inspector-table');
+  const _btnSave = /** @type {HTMLInputElement} */document.getElementById('button-save');
+  vscode.postMessage({
+    type: 'change-editor',
+    idx : _ins.id,
+    id: id,
+    value : _editor.type==='checkbox' ? _editor.checked : _editor.value
+  });
+  // @ts-ignore
+  _btnSave.disabled = false;
+}
+
+function doSavePrepare() {
+  vscode.postMessage({
+    type: 'save-data-prepare'
+  });
+}
+
+function viewSqlText(/**@type {string}*/sql, /**@type {Number}*/countRow) {
+  const _textarea = /** @type {HTMLElement} */document.getElementById('sql-text');
+  // @ts-ignore
+  _textarea.value = sql;
+  // @ts-ignore
+  _textarea.rows = countRow;
+  SelectTab(8);
+}
+
+function doExecSql() {
+  const _textarea = /** @type {HTMLElement} */document.getElementById('sql-text');
+  vscode.postMessage({
+    type: 'execute-sql',
+    // @ts-ignore
+    text: _textarea.value
+  });
+}
+
